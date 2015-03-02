cdebcbbeb58a8e0211e47aa3f619c1482cf5945b
Milen Dyankov <MilenDyankov@gmail.com>
2015-03-02 19:45:42 +0100

    Updated JDepend stat script to show components

--- stats/jdepend.sh
+++ stats/jdepend.sh
@@ -4,17 +4,31 @@ REPORT='report'
 REPORT_DIR="$BASEDIR/reports"
 LIB_DIR="$BASEDIR/lib"
 
+COMPONENTS="com.forest.usecase.identity,\
+com.forest.usecase.catalog,\
+com.forest.usecase.ecommerce,\
+com.forest.store.wiring.identity,\
+com.forest.store.wiring.catalog,\
+com.forest.store.wiring.ecommerce,\
+com.forest.store.ui,\
+com.forest.shipment.wiring,\
+com.forest.shipment.ui,\
+com.forest.payment.services,\
+com.forest.persistence,\
+com.forest.model"
+
+
 mkdir -p $REPORT_DIR
 
 case $1 in
     'gui') 
-	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.swingui.JDepend  $BASEDIR/.. 
+	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.swingui.JDepend -components $COMPONENTS $BASEDIR/.. 
 	;;
     'xml')
-	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.xmlui.JDepend  $BASEDIR/.. 
+	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.xmlui.JDepend -components $COMPONENTS $BASEDIR/.. 
 	;;
     "$REPORT")
-	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.xmlui.JDepend  $BASEDIR/.. > $REPORT_DIR/jdepend.xml
+	java -cp $LIB_DIR/jdepend-2.9.1.jar:$LIB_DIR jdepend.xmlui.JDepend -components $COMPONENTS $BASEDIR/.. > $REPORT_DIR/jdepend.xml
 	xsltproc $LIB_DIR/jdepend2dot.xsl $REPORT_DIR/jdepend.xml > $REPORT_DIR/jdepend.dot
 	dot -Tpng $REPORT_DIR/jdepend.dot > $REPORT_DIR/jdepend.png
 	xsltproc $LIB_DIR/jdepend2html_img.xsl $REPORT_DIR/jdepend.xml > $REPORT_DIR/jdepend.html 
